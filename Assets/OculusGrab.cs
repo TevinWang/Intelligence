29f371b542759f2780350dc4cd2d5e3d1bd9352c
TevinWang <tevin.wang@warriorlife.net>
2020-11-30 09:08:09 -0800

    Intelligence v1

---
+++ Assets/OculusGrab.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class OculusGrab : MonoBehaviour {
+
+
+// identifying objects
+public GameObject CollidingObject;
+public GameObject objectInHand;
+
+
+// trigger functions after adding trigger zones to controllers and adding script to controllers
+public void OnTriggerEnter(Collider other) //picking up objects with rigidbodies
+    {
+    if(other.gameObject.GetComponent<Rigidbody>())
+        {
+            CollidingObject = other.gameObject;
+        }
+    }
+public void OnTriggerExit(Collider other) // releasing those objects with rigidbodies
+    {
+        CollidingObject = null;
+    }
+
+
+void Update () // refreshing program confirms trigger pressure and determines whether holding or releasing object
+    {
+    if(Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") > 0.2f && CollidingObject)
+        {
+            GrabObject();
+        }
+    if(Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") < 0.2f && objectInHand)
+        {
+            ReleaseObject();
+        }
+    }
+
+
+public void GrabObject() //create parentchild relationship between object and hand so object follows hand
+    {
+        objectInHand = CollidingObject;
+        objectInHand.transform.SetParent (this.transform);
+        objectInHand.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+
+private void ReleaseObject() //removing parentchild relationship so you drop the object
+    {
+        objectInHand.GetComponent<Rigidbody>().isKinematic = false;
+        objectInHand.transform.SetParent (null);
+        objectInHand = null;
+    }
+}
---
+++ Assets/speed.cs
@@ -0,0 +1,18 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+ 
+ public class speed : MonoBehaviour {
+
+     public GameObject speedText;
+     
+
+     void Update () {
+         var speed = GetComponent<Rigidbody>().velocity.magnitude;
+        TextMesh textObject = speedText.GetComponent<TextMesh>();
+        textObject.text = "speed:" + GetComponent<Rigidbody>().velocity.magnitude + "m/s";
+         Debug.Log("speed:" + GetComponent<Rigidbody>().velocity.magnitude + "m/s"); 
+     }
+ }
+ 
